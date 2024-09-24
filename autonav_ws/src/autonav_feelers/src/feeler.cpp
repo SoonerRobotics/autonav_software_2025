6986147582e45d0060f4933ab21ed07163f04bb4
danielbrownmsm <daniel.brown@msmrockets.org>
2024-09-24 16:09:20 -0500

    translate feeler.py's update() function into C++

--- autonav_ws/src/autonav_feelers/src/feeler.cpp
+++ autonav_ws/src/autonav_feelers/src/feeler.cpp
@@ -1,16 +1,16 @@
 #pragma once
 
+#include <cmath>
+
+//TODO move to a utils.cpp file or something
+int sign(int x) {
+    if (x < 0) {
+        return -1;
+    }
+    return 1;
+}
+
 class Feeler {
-private:
-    int x = 0;
-    int y = 0;
-    double length = 0.0;
-
-    int original_x = 0;
-    int original_y = 0;
-    double original_length = 0.0;
-
-    cv::Scalar color;
 public:
     Feeler();
     ~Feeler();
@@ -35,7 +35,17 @@ public:
 
     def add(); //TODO
     def sub(); //TODO
-}
+private:
+    int x = 0
+    int y = 0;
+    double length = 0.0;
+
+    int original_x = 0;
+    int original_y = 0;
+    double original_length = 0.0;
+
+    cv::Scalar color;
+};
 
 /**
  * @return x coordinate of the end of the feeler
@@ -113,6 +123,67 @@ void Feeler::setXY(int x, int y) {
  */
 void Feeler::update(cv::Mat mask) {
     //TODO
+    int x_ = 0;
+    int y_ = 0;
+
+    int prev_x = 0;
+    int prev_y = 0;
+
+    double new_y = 0;
+    double new_x = 0;
+
+    int x_dir = sign(this->original_x);
+    int y_dir = sign(this->original_y);
+
+    double slope = 0;
+    bool slopeIsInfinity = false;
+    if (this->original_y != 0) {
+        slope = this->original_y / this->original_y;
+    } else {
+        slopeIsInfinity = true;
+    }
+
+    // vertical line, just need to move along the y-axis
+    if (slopeIsInfinity) {
+        y_ += 1;
+    } else if (slope == 0) {
+        x_ += 1;
+    } else if (abs(slope) <= 1) {
+        // if slope is shallow, make x the independent variable
+        // get the y as a function of x
+        new_y = abs(slope) * x_;
+
+        // if the new y is higher than the previous one
+        if ((new_y - prev_y) > 0) {
+            y_ += 1; // then go up by 1 y
+        }
+
+        x_ += 1;
+        prev_y = y_;
+    } else { // slope is steep, do y as independent variable
+        // get x as a function of y
+        new_x = abs(1/slope) * y_;
+
+        // and then if the new x is larger than the old one
+        if ((new_x - prev_x) > 0) {
+            x_ += 1; // go up by one
+        }
+
+        y_ += 1;
+        prev_x = x_;
+    }
+    
+    std::vector<double> coords = self.centerCoordinates(x_*x_dir, y_*y_dir);
+
+    // if any of the pixel's color values (in RGB I think) are > 0 then
+    if (mask[coords[1], coords[0]].any() > 0) {
+        // that is our new length
+        this->setXY(this->x_*x_dir, this->y_*y_dir);
+        return; // and quit so we don't keep looping 'cause we found an obstacle
+    } else if (abs(x_) > abs(this->original_x) || abs(y_) > abs(this->original_y)) { // if we're past our original farthest point
+        this->setXY(self.original_x, self.original_y); // then we found no obstacle, and should stop looping
+        return;
+    }
 }
 
 /**
@@ -120,7 +191,9 @@ void Feeler::update(cv::Mat mask) {
  * @param image an image that the feeler can be drawn on
  */
 void Feeler::draw(cv::Mat image) {
-    //TODO
+    startPt = Point(this->centerCoordinates(0, 0));
+    endPt = Point(this->centerCoordinates(this->x, this->y));
+    cv2::line(image, startPt, endPt, this->color, 5); // thickness of 5
 }
 
 // def add(); //TODO
--- autonav_ws/src/autonav_feelers/src/feeler_node.cpp
+++ autonav_ws/src/autonav_feelers/src/feeler_node.cpp
@@ -1,3 +1,64 @@
 #pragma once
 
 #include "feeler.cpp" //TODO
+
+class FeelerNode : public rclcpp::Node {
+public:
+    //TODO waypoints file reading code
+
+    FiltersNode() : AutoNav::Node("autonav_feelers");
+    ~FiltersNode() {}
+
+    void init() override {
+        //TODO publishers / subscribers
+
+        set_device_state(AutoNav::DeviceState::OPERATING);
+    }
+
+    json get_default_config() override {
+        FeelerConfig newConfig;
+        return newConfig;
+        //TOOD
+    }
+
+    /**
+     * Main callback to command motor output upon receiving the combined image from combination.py
+     * @param image a compressedimage message with the combined transformations of all 4 cameras
+     */
+    void on_image_received(const msg::CompressedImage::SharedPtr image) {
+        // if we aren't in autonomous
+        if (system_state != AutoNav::SystemState::AUTONOMOUS) {
+            return; // return because we don't need to do anything
+        }
+
+        // calculate new length of every new feeler
+        for (Feeler feeler : this->feelers) {
+            feeler.update(mask); //TODO
+        }
+
+        //TODO
+
+        // TODO safety lights
+        
+        // Make the motor output message
+        autonav_messages::msg::MotorInput msg;
+        msg.forward_velocity = 0.0;
+        msg.sideways_velocity = 0.0;
+        msg.angular_velocity = 0.0;
+
+        motorPublisher->publish(msg);
+    }
+
+    void on_debug_received(image); //TODO
+    void on_position_received(image); //TODO
+    
+    void main();
+
+private:
+    std::vector<Feeler> feelers;
+    //TODO ultrasonics, GPS
+    //TODO subscribers
+    //TODO publishers
+    rclcpp::Subscription<autonav_messages::msg::Position>::SharedPtr positionSubscriber;
+    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr imageSubscriber;
+};
