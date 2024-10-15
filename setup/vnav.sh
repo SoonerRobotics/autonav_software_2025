e05761c263b52aec116dcb3f1479a955f3bada8e
danielbrownmsm <daniel.brown@msmrockets.org>
2024-10-15 18:44:32 -0500

    actually fix the setup script fr trust bro this time

--- setup/vnav.sh
+++ setup/vnav.sh
@@ -6,6 +6,7 @@ unzip -o vectornav.zip -d /usr/local/vectornav
 rm vectornav.zip
 
 # Install vectornav
+sudo apt install python3-pybind11
 python3 -m pip config set global.break-system-packages true
 cd /usr/local/vectornav/python
 sudo python3 setup.py install -q --break-system-packages
