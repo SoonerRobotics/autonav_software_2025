90ae83363dfd751efc70d0b7aeecc14640de1ce5
Dylan Zemlin <dylansmrw@gmail.com>
2024-10-29 19:28:55 -0500

    Finished configuration system

--- autonav_ws/src/autonav_example_py/src/example.py
+++ autonav_ws/src/autonav_example_py/src/example.py
@@ -3,12 +3,24 @@
 from autonav_shared.node import Node
 from autonav_shared.types import LogLevel, DeviceState, SystemState
 import rclpy
-import time
 
 
-class Example(Node):
+class ExamplePyConfig:
+    def __init__(self):
+        self.alpha = 0.5
+        self.beta = "Hello"
+        self.gamma = 42
+        self.delta = True
+        self.epsilon = [0.1, 0.2, 0.3]
+        self.zeta = ["A", "B", "C"]
+        self.eta = {"A": 1, "A": 2, "A": 3}
+        self.rid = 2
+
+
+class ExamplePy(Node):
     def __init__(self):
         super().__init__("autonav_example_py")
+        self.write_config(ExamplePyConfig())
         
         self.log("Hello from ExamplePy", LogLevel.DEBUG)
         self.log("Hello from ExamplePy", LogLevel.INFO)
@@ -22,10 +34,14 @@ class Example(Node):
 
         self.perf_start("example")
         self.perf_stop("example", True)
+
+        self.request_all_configs()
+
+        self.log(f"Beta (CONFIG): {self.config.beta}", LogLevel.DEBUG)
         
 def main():
     rclpy.init()
-    example = Example()
+    example = ExamplePy()
     rclpy.spin(example)
     rclpy.shutdown()
 
