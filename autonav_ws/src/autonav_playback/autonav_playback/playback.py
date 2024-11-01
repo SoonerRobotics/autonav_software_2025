d650db3763f7a849ccd24650a75d866b3bf0c1c3
SquidwardWzRd <ghair244@gmail.com>
2024-11-01 00:16:37 +0000

    subscribers

--- autonav_ws/src/autonav_playback/autonav_playback/playback.py
+++ autonav_ws/src/autonav_playback/autonav_playback/playback.py
@@ -1,27 +1,63 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
-from autonav_msgs.msg import DeviceState, SystemState
+from std_msgs.msg import *
+from sensor_msgs.msg import CompressedImage
+from autonav_msgs.msg import *
 
 
 from datetime import datetime
 import os
 
+import subprocess
+
 class playback(Node):
+    """
+    This Node does some stuff :)
+    """
     
     def __init__(self):
         super().__init__('autonav_playback')
         
-        self.QOS = 10
+        self.file = None
         
-        self.LOGPATH = "~/Documents/AutoNav/Logs"
+        self.QOS = 10
         self.home_dir = os.path.expanduser("~")
-        self.system_state = None
+        self.system_state = 0
         
         # Topic Listeners
         self.systemStateSub = self.create_subscription(SystemState, 'autonav/shared/system', self.systemStateCallback, self.QOS)
         self.deviceStateSub = self.create_subscription(DeviceState, 'autonav/shared/device', self.deviceStateCallback, self.QOS)
+        
+        # IMU is still TBD
+        self.imu_subscriber  = self.create_subscription(IMUData, '/autonav/imu', self.imu_feedback, self.QOS)
+        self.gps_subscriber = self.create_subscription(GPSFeedback, '/autonav/gps', self.gps_feedback, self.QOS)
+        self.feedback_subscriber = self.create_subscription(MotorFeedback, '/autonav/motorfeedback', self.motor_feedback, self.QOS)
+        self.position_subscriber = self.create_subscription(Position, '/autonav/position', self.position_feedback, self.QOS)
+        self.nuc_subscriber = self.create_subscription(NUCStatistics, '/autonav/statistics', self.nuc_feedback, self.QOS)
+        self.ultrasonics_subscriber = self.create_subscription(Ultrasonic, '/autonav/ultrasonics', self.ultrasonic_feedback, self.QOS)
+        self.conbus_subscriber = self.create_subscription(Conbus, '/autonav/CONbus', self.conbus_feedback, self.QOS)
+        self.safetylight_subscriber = self.create_subscription(SafetyLights, '/autonav/safety_lights', self.safetylight_feedback, self.QOS)
+        self.performance_subscriber = self.create_subscription(Performance, '/autonav/performance', self.performance_feedback, self.QOS)
+        
+        
+        
+        # TO-DO
+        # Prolly even more camera stuff
+        #self.camera_subscriber_left 
+        #self.camera_subscriber_right
+        #self.astar_subscriber
+        
+        # self.camera1 = self.create_subscription(CompressedImg, 'autonav/camera/1, self.cameraCallback, self.QOS)
+        # self.camera2 = self.create_subscription(CompressedImg, 'autonav/camera/2, self.cameraCallback, self.QOS)
+        # self.camera3 = self.create_subscription(CompressedImg, 'autonav/camera/3, self.cameraCallback, self.QOS)
+        # self.camera4 = self.create_subscription(CompressedImg, 'autonav/camera/4, self.cameraCallback, self.QOS)
+        
+        
+        # FFmpeg record video
+        # ffmpeg -f avfoundation -framerate 30 -i "0:1" output.mp4
+        # Might need to change avfoundation, framerate and -i
+        
         
         # Silly Goofy Code
         self.__topicList = []
@@ -60,10 +96,14 @@ class playback(Node):
     
     # Serious Stuff below ->
     def systemStateCallback(self, msg):
-        print(msg.data)
+        # ?
+        print(msg.state)
+        
+        self.system_state = msg.state
+        
     
     def deviceStateCallback(self, msg):
-        print(msg.data)
+        self.write_file(f"{self.makeTimestamp}, ENTRY_DEVICE, {msg.device}, {msg.state}")
     
     
     def makeTimestamp(self) -> str:
@@ -71,8 +111,58 @@ class playback(Node):
         frmt = time.strftime("%Y-%m-%d_%H-%M-%S")
         return frmt
     
-    def checkSystemState(self):
-        pass
+    def write_file(self, msg):
+        if self.file == None:
+            return
+        
+        self.file.write(msg + "\n")
+    
+    
+    def create_entry(self):
+        stateFrmt = "autonomous" if self.system_state == 0 else "manual"
+        filename = f"{stateFrmt}_{self.makeTimestamp()}"
+        
+        BASE_PATH = os.path.join(self.home_dir, "Documents", "AutoNav", "Logs", filename)
+        os.makedirs(BASE_PATH, exist_ok=True)
+        
+        self.file = open(os.path.join(BASE_PATH, "log.csv"), "w")
+        self.file.write("timestamp, type\n")
+    
+    def close_entry(self):
+        if self.file is None:
+            return
+        
+        self.file.close()
+        self.file = None
+        
+    def imu_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_IMU, {msg.yaw}, {msg.pitch}, {msg.roll}, {msg.accel_x}, {msg.accel_y}, {msg.accel_z}, {msg.angular_x}, {msg.angular_y}, {msg.angular_z}")
+    
+    def gps_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_GPS, {msg.latitude}, {msg.longitude}, {msg.altitude}, {msg.gps_fix}, {msg.is_locked}, {msg.satellites}")
+    
+    def motor_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_FEEDBACK, {msg.delta_x}, {msg.delta_y}, {msg.delta_theta}")
+    
+    def position_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_POSITION, {msg.x}, {msg.y}, {msg.theta}, {msg.latitude}, {msg.longitude}")
+        
+    def nuc_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_PERFORMACE", {msg.timestamp}, {msg.cpu_percentage}, {msg.ram_usage}, {msg.disk_usage}, {msg.gpu_usage})
+    
+    def ultrasonic_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_ULTRASONIC, {msg.id}, {msg.distance}")
+        
+    def conbus_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_CONBUS, {msg.id}, {msg.data}, {msg.iterator}")
+    
+    def safetylight_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_SAFETYLIGHT, {msg.autonomous}, {msg.red}, {msg.green}, {msg.blue}")
+    
+    def performance_feedback(self, msg):
+        self.write_file(f"{self.makeTimestamp}, ENTRY_PERFORMANCE, {msg.name}, {msg.duration}")
+    
+    
     
         
 def main(args=None):
@@ -81,10 +171,6 @@ def main(args=None):
     playback_sub = playback()
 
     rclpy.spin(playback_sub)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     playback_sub.destroy_node()
     rclpy.shutdown()
 
--- autonav_ws/src/autonav_playback/autonav_playback/testTopicsSpam.py
+++ autonav_ws/src/autonav_playback/autonav_playback/testTopicsSpam.py
@@ -1,9 +1,23 @@
 ### Flood Ros with topics in order to test topic_pub node
-
+import sys
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from autonav_msgs.msg import SystemState
+from autonav_msgs.srv import SetSystemState
+
+class MinimalService(Node):
+
+    def __init__(self):
+        super().__init__('minimal_service')
+        self.srv = self.create_service(SetSystemState, 'set_system_state', self.set_systemstate_callback)
+
+    def set_systemstate_callback(self, request, response):
+        response.ok = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        return response
 
 
 class SpamPublisher(Node):
@@ -13,15 +27,20 @@ class SpamPublisher(Node):
         self.publisher_ = self.create_publisher(String, 'topic1', 10)
         self.publisher_2 = self.create_publisher(String, 'topic2', 10)
         self.publisher_3 = self.create_publisher(String, 'topic3', 10)
-        self.publisher_4 = self.create_publisher(String, 'topic4', 10)
-        self.publisher_5 = self.create_publisher(String, 'topic5', 10)
-        self.publisher_6 = self.create_publisher(String, 'topic6', 10)
-        self.publisher_7 = self.create_publisher(String, 'topic7', 10)
-        self.publisher_8 = self.create_publisher(String, 'topic8', 10)
-        self.publisher_9 = self.create_publisher(String, 'topic9', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
+        
+        self.cli = self.create_client(SetSystemState, 'add_two_ints')
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        self.req = SetSystemState.Request()
+        
+    def send_request(self, a, b):
+        self.req.state = a
+        self.req.mobility = b
+        return self.cli.call_async(self.req)
+        
 
     def timer_callback(self):
         msg = String()
@@ -29,12 +48,6 @@ class SpamPublisher(Node):
         self.publisher_.publish(msg)
         self.publisher_2.publish(msg)
         self.publisher_3.publish(msg)
-        self.publisher_4.publish(msg)
-        self.publisher_5.publish(msg)
-        self.publisher_6.publish(msg)
-        self.publisher_7.publish(msg)
-        self.publisher_8.publish(msg)
-        self.publisher_9.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
@@ -43,12 +56,14 @@ def main(args=None):
     rclpy.init(args=args)
 
     spam_publisher = SpamPublisher()
+    future = spam_publisher.send_request(int(sys.argv[1]), int(sys.argv[False]))
+    rclpy.spin_until_future_complete(spam_publisher, future)
+    
+    response = future.result()
+    spam_publisher.get_logger().info(
+        'Result of add_two_ints: for %d + %d = %d' %
+        (int(sys.argv[1]), int(sys.argv[False]), response.ok))
 
-    rclpy.spin(spam_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     spam_publisher.destroy_node()
     rclpy.shutdown()
 
