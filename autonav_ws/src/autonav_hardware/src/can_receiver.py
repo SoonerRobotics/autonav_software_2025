bdf10c6753be7f78fad448f7e44b680b45adf5d1
antonioc76 <antoniochappell@protonmail.com>
2025-01-15 19:45:33 -0600

    adding basic receiver for testing, will remove once testing is complete

---
+++ autonav_ws/src/autonav_hardware/src/can_receiver.py
@@ -0,0 +1,11 @@
+import can
+
+dev_channel = input("enter canable channel:")
+
+my_can_bus = can.ThreadSafeBus(
+    bustype="slcan", channel=dev_channel, bitrate=100000
+)
+
+with my_can_bus as bus:
+    for msg in bus:
+        print(msg.data)
