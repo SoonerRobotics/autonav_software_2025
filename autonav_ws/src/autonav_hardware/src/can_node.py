b591885ad10be6c8d0031498438b794a96e9c78a
antonioc76 <antoniochappell@protonmail.com>
2025-01-20 18:50:51 -0600

    90% finished with CAN node testing. Tested sending and receiving, both work. Just need to test a few more throughput to ros2 publishers

--- autonav_ws/src/autonav_hardware/src/can_node.py
+++ autonav_ws/src/autonav_hardware/src/can_node.py
@@ -9,6 +9,7 @@ import can
 import threading
 import struct
 from ctypes import Structure, c_bool, c_uint8
+import binascii
 
 arbitration_ids = {
     "EStop": 0,
@@ -28,18 +29,17 @@ arbitration_ids = {
 
 class SafetyLightsPacket(Structure):
     _fields_ = [
-        ("autonomous", c_bool, 1),
-        ("mode", c_uint8, 7),
-        ("brightness", c_uint8, 8),
-        ("red", c_uint8, 8),
-        ("green", c_uint8, 8),
-        ("blue", c_uint8, 8),
-        ("blink_period", c_uint8, 8)
+        ("mode", c_uint8),
+        ("brightness", c_uint8),
+        ("red", c_uint8),
+        ("green", c_uint8),
+        ("blue", c_uint8),
+        ("blink_period", c_uint8)
     ]
 
 class CanConfig:
     def __init__(self):
-        self.canable_filepath = "/dev/ttyACM3"
+        self.canable_filepath = "/dev/ttyACM0"
         self.odom_feedback_scaler = 10000
         self.linear_pid_scaling_factor = 1000
         self.angular_pid_scaling_factor = 1000
@@ -114,9 +114,11 @@ class CanNode(Node):
 
     def init(self):
         # can threading
+        self.log("initializing CAN node", LogLevel.DEBUG)
         self.canTimer = self.create_timer(0.5, self.can_worker)
         self.canReadThread = threading.Thread(target=self.can_thread_worker)
         self.canReadThread.daemon = True
+        self.canReadThread.start()
 
 
     def can_worker(self):
@@ -139,6 +141,7 @@ class CanNode(Node):
 
 
     def can_thread_worker(self):
+        self.log("can thread worker working", LogLevel.DEBUG)
         while rclpy.ok():
             if self.get_device_state() != DeviceState.READY and self.get_device_state() != DeviceState.OPERATING:
                 continue
@@ -152,37 +155,49 @@ class CanNode(Node):
     
 
     def onCanMessageReceived(self, msg):
+        self.log("CAN message received", LogLevel.DEBUG)
         arbitration_id = msg.arbitration_id
-        if arbitration_id == arbitration_ids["Estop"]:
+        self.log(f"{arbitration_id}", LogLevel.DEBUG)
+        if arbitration_id == arbitration_ids["EStop"]:
+            self.log("EStop", LogLevel.DEBUG)
             self.set_mobility(False)
 
         if arbitration_id == arbitration_ids["MobilityStart"]:
+            self.log("Mobility Start", LogLevel.DEBUG)
             self.set_mobility(True)
         
         if arbitration_id == arbitration_ids["MobilityStop"]:
+            self.log("Mobility Stop", LogLevel.DEBUG)
             self.set_mobility(False)
 
         if arbitration_id == arbitration_ids["OdometryFeedback"]:
+            self.log("Odom Feedback", LogLevel.DEBUG)
             self.publish_odom_feedback(msg)
 
         if arbitration_id == arbitration_ids["LinearPIDStatistics"]:
+            self.log("Linear PID Stats", LogLevel.DEBUG)
             self.publish_linear_pid_statistics(msg)
     
         if arbitration_id == arbitration_ids["AngularPIDStatistics"]:
+            self.log("Angular PID Stats", LogLevel.DEBUG)
             self.publish_angular_pid_statistics(msg)
 
         if arbitration_id == arbitration_ids["MotorStatisticsFrontMotors"]:
+            self.log("Motor Statistics Front Motors", LogLevel.DEBUG)
             self.publish_motor_statistics_front_motors(msg)
         
         if arbitration_id == arbitration_ids["MotorStatisticsBackMotors"]:
+            self.log("Motor Statistics Back Motors", LogLevel.DEBUG)
             self.publish_motor_statistics_back_motors(msg)
 
         if arbitration_id >= arbitration_ids["ConbusLowerBound"] and arbitration_id < arbitration_ids["ConbusUpperBound"]:
+            self.log("Conbus", LogLevel.DEBUG)
             self.publish_conbus(msg)
 
 
     def publish_odom_feedback(self, msg):
-        delta_x, delta_y, delta_theta = struct.unpack('hhh', msg.data)
+        self.log(f"{msg.data.hex()}", LogLevel.DEBUG)
+        delta_x, delta_y, delta_theta = struct.unpack('>hhh', msg.data)
         motor_feedback_msg = MotorFeedback()
         motor_feedback_msg.delta_x = delta_x / self.config.get("odom_feedback_scaler")
         motor_feedback_msg.delta_y = delta_y / self.config.get("odom_feedback_scaler")
@@ -192,7 +207,7 @@ class CanNode(Node):
 
 
     def publish_linear_pid_statistics(self, msg):
-        forward_velocity, forward_velocity_setpoint, sideways_velocity, sideways_velocity_setpoint = struct.unpack("hhhh", msg.data)
+        forward_velocity, forward_velocity_setpoint, sideways_velocity, sideways_velocity_setpoint = struct.unpack(">hhhh", msg.data)
         linear_pid_statistics_msg = LinearPIDStatistics()
         linear_pid_statistics_msg.forward_velocity = forward_velocity
         linear_pid_statistics_msg.forward_velocity_setpoint - forward_velocity_setpoint
@@ -203,7 +218,7 @@ class CanNode(Node):
 
 
     def publish_angular_pid_statistics(self, msg):
-        angular_velocity, angular_velocity_setpoint = struct.unpack("hh", msg.data)
+        angular_velocity, angular_velocity_setpoint = struct.unpack(">hh", msg.data)
         angular_pid_statistics_msg = AngularPIDStatistics()
         angular_pid_statistics_msg.angular_velocity = angular_velocity
         angular_pid_statistics_msg.angular_velocity_setpoint = angular_velocity_setpoint
@@ -212,7 +227,7 @@ class CanNode(Node):
 
 
     def publish_motor_statistics_front_motors(self, msg):
-        left_drive_motor_output, left_steer_motor_output, right_drive_motor_output, right_steer_motor_output= struct.unpack("hh", msg.data)
+        left_drive_motor_output, left_steer_motor_output, right_drive_motor_output, right_steer_motor_output= struct.unpack(">hhhh", msg.data)
         motor_statistics_front_motors = MotorStatisticsFrontMotors()
         motor_statistics_front_motors.left_drive_motor_output = left_drive_motor_output
         motor_statistics_front_motors.left_steer_motor_output = left_steer_motor_output
@@ -223,7 +238,7 @@ class CanNode(Node):
 
 
     def publish_motor_statistics_back_motors(self, msg):
-        left_drive_motor_output, left_steer_motor_output, right_drive_motor_output, right_steer_motor_output= struct.unpack("hh", msg.data)
+        left_drive_motor_output, left_steer_motor_output, right_drive_motor_output, right_steer_motor_output= struct.unpack(">hhhh", msg.data)
         motor_statistics_back_motors = MotorStatisticsFrontMotors()
         motor_statistics_back_motors.left_drive_motor_output = left_drive_motor_output
         motor_statistics_back_motors.left_steer_motor_output = left_steer_motor_output
@@ -243,16 +258,18 @@ class CanNode(Node):
     # subscriber callbacks
     def on_safety_lights_received(self, msg:SafetyLights):
         safety_lights_packet = SafetyLightsPacket()
-        safety_lights_packet.autonomous = msg.autonomous
         safety_lights_packet.mode = msg.mode
+        safety_lights_packet.brightness = msg.brightness
         safety_lights_packet.red = msg.red
         safety_lights_packet.green = msg.green
         safety_lights_packet.blue = msg.blue
         safety_lights_packet.blink_period = msg.blink_period
 
         data = bytes(safety_lights_packet)
-        can_msg = can.Message(arbitration_id=arbitration_ids["SafetyLightsCommand"], data=data)
+        self.log(f"{data.hex()}", LogLevel.DEBUG)
 
+        can_msg = can.Message(arbitration_id=arbitration_ids["SafetyLightsCommand"], data=data)
+        
         try:
             self.can.send(can_msg)
         except can.CanError:
@@ -262,8 +279,10 @@ class CanNode(Node):
     def on_motor_input_received(self, msg:MotorInput):
         if self.get_device_state() != DeviceState.OPERATING:
             return
-        data = struct.pack("hhh", int(msg.forward_velocity * 10000), int(msg.sideways_velocity * 10000), int(msg.angular_velocity * 10000))
-        can_msg = can.message(arbitration_id = arbitration_ids["MotorsCommand"], data = data)
+        data = struct.pack(">hhh", int(msg.forward_velocity * 1000), int(msg.sideways_velocity * 1000), int(msg.angular_velocity * 1000))
+        can_msg = can.Message(arbitration_id = arbitration_ids["MotorsCommand"], data = data)
+        self.log(f"{can_msg.data.hex()}", LogLevel.DEBUG)
+
         try:
             self.can.send(can_msg)
         except can.CanError:
