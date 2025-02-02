99ea1d109410afe54d24fca0f40f3e972c3a6455
SquidwardWzRd <ghair244@gmail.com>
2025-02-02 00:13:09 +0000

    cpu, memory, and senor info

--- autonav_ws/autonav_performance_node/src/performance.py
+++ autonav_ws/autonav_performance_node/src/performance.py
@@ -7,12 +7,49 @@ from autonav_shared.types import LogLevel, DeviceState, SystemState
 
 import psutil
 
+class Config():
+    
+    def __init__(self):
+        self.CELCIUS = False
+
 class PerformanceNode(Node):
     
     def __init__(self):
-        self.log("sussy baka")
+        # Initiliza config
+        self.config = Config()
         
+        # setup cpu and mem
+        self.cpu_utilization = psutil.cpu_percent()
+        self.update_cpu()
+        self.memory = psutil.virtual_memory()
+        
+    
+    def query_temps(self):
+        if self.config.CELCUIS:
+            # Send in Celcius
+            return psutil.sensors_temperatures()
+        else:
+            # Send in American
+            return psutil.sensors_temperatures(True)
+    
+    def query_battery(self):
+        battery = psutil.sensors_battery()
+        if battery = None:
+            return
+        else:
+            return battery
+        
+    
+    def update_cpu(self, interval: float = 0.0):
+        i = None
+        if interval <= 0:
+            i = 0.0
+            self.cpu_utilization = psutil.cpu_percent(i)
+            return
+        self.cpu_utilization = psutil.cpu_percent(interval)
 
+    def update_memory(self):
+        self.memory = psutil.virtual_memory()
 
 def main(args=None):
     rclpy.init(args=args)
