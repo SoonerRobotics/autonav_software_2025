98ac437ce86981f652d91257fb761a8fcd58148c
SCR <ouscr10@gmail.com>
2026-04-23 19:23:40 -0500

    no work :(

--- autonav_ws/src/autonav_hardware/src/test_can_converter.py
+++ autonav_ws/src/autonav_hardware/src/test_can_converter.py
@@ -3,7 +3,7 @@ import serial
 import can
 import struct
 import time
-from ctypes import Structure, c_uint16, c_uint8, c_bool
+from ctypes import Structure, c_uint16, c_uint8, c_bool, c_int16, c_short
 
 SCALE = 1000
 
@@ -20,12 +20,12 @@ controller.init()
 #FIXME bitrate is 125 kbps according to the spec, but rn Micheal's firmware is the same as last year (i.e. 100 kbps)
 can_net = can.ThreadSafeBus(bustype="slcan", channel="/dev/ttyACM0", bitrate=100_000)
 
-# class MotorInputPacket(Structure):
-#     _fields_ = [
-#         ("forward_velocity", c_uint16, 16),
-#         ("sideways_velocity", c_uint16, 16),
-#         ("angular_velocity", c_uint16, 16),
-#     ]
+class MotorInputPacket(Structure):
+    _fields_ = [
+        ("forward_velocity", c_short, 16),
+        ("sideways_velocity", c_short, 16),
+        ("angular_velocity", c_short, 16),
+    ]
 
 try:
     while True:
@@ -36,32 +36,35 @@ try:
         sideways = controller.get_axis(0)
         angular = controller.get_axis(2)
 
-        # motor_input_packer = MotorInputPacket()
-        # motor_input_packer.forward_velocity = int(forward * SCALE)
-        # motor_input_packer.sideways_velocity = int(sideways * SCALE)
-        # motor_input_packer.angular_velocity = int(angular * SCALE)
+        motor_input_packer = MotorInputPacket()
+        motor_input_packer.forward_velocity = int(forward * SCALE * 100)
+        motor_input_packer.sideways_velocity = int(sideways * SCALE * 100)
+        motor_input_packer.angular_velocity = int(angular * SCALE / 100)
 
-        print(f"{forward:.2f} | {sideways:.2f} | {angular:.2f}")
+        # print(f"{forward:.2f} | {sideways:.2f} | {angular:.2f}")
 
-        # data = bytes(motor_input_packer)
-        data = struct.pack("<hhh", int(forward * SCALE), int(sideways * SCALE), int(angular * SCALE))
-        can_msg = can.Message(arbitration_id=10, data=data)
+        data = bytes(motor_input_packer)
+        # data = struct.pack("<hhh", int(forward * SCALE), int(sideways * SCALE), int(angular * SCALE))
+        data = struct.pack("<hhh", 25000, 0, 0)
+        can_msg = can.Message(arbitration_id=0xA, data=data)
+        # other_can_msg = can.Message(arbitration_id=0x9)
         
         try:
             can_net.send(can_msg)
+            # can_net.send(other_can_msg)
         except AttributeError:
             print("Error writing to CAN",)
         except can.CanError:
             print("2 Error writing to CAN")
 
-        try:
-            msg = can_net.recv(timeout=0.01)
-            if msg is not None:
-                print(msg.arbitration_id)
-        except can.CanError:
-            print("CAN ERROR!!! IN RECV!!!")
+        # try:
+        #     msg = can_net.recv(timeout=0.01)
+        #     if msg is not None:
+        #         print(msg.arbitration_id)
+        # except can.CanError:
+        #     print("CAN ERROR!!! IN RECV!!!")
         
-        timer.tick(100)
+        # timer.tick(100)
 except Exception as e:
     print(e)
     can_net.shutdown()
